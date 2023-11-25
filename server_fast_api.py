cf0cede65220ce9d4134f67ce38725d411b41425
yusuf emad <yusufy2004@gmail.com>
2023-11-25 15:09:07 +0200

    added image extract and modified buildspec

--- server_fast_api.py
+++ server_fast_api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File
+import base64
 import os
 import uvicorn
 from drug_interactions import *
@@ -30,5 +31,49 @@ async def drug_interactions(drugs: dict):
         }
     except:
         raise HTTPException(status_code=400, detail="Invalid payload. 'drugs' should be a list.")
+
+@app.post("/image-extract")
+async def upload_and_process_image(image: UploadFile = File(...)):
+    # Process the uploaded image
+    api_key = "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"
+    image_bytes = await image.read()
+
+    # Convert binary image data to base64
+    base64_rotated_image = base64.b64encode(image_bytes).decode('utf-8')
+
+    # You can now use base64_rotated_image for further processing or sending to the OpenAI API
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You will take a medicine box image from the user, and you will extract the text and the colors from the image and you will describe the medicine and how to use it.\nYour response should be a json"
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "the image I provided is a picture of a medicine box respond with json with key 'text' text is any text visible inside the image and  key colors is any colors visible in the image' and a key description with any description of the medicine and how to use. like this {'text': 'text here', 'colors': ['red', 'white'], 'description': 'description here'}"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpg;base64,{base64_rotated_image}"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
+    headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        }
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    content = response.json()['choices'][0]['message']['content']
+
+    return content
 
 uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
